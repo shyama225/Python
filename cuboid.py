8051f393b0d3406b2769693fb9158be398a0f238
shyama225 <shyama2002feb@gmail.com>
2025-01-09 13:31:38 +0530

    Add files via upload

---
+++ cuboid.py
@@ -0,0 +1,4 @@
+def cuboid_area(l, w, h):
+    area = 2 * (l * w + l * h+ w * h)
+    return area
+c_area = cuboid_area(2, 1, 3)
---
+++ sphere.py
@@ -0,0 +1,5 @@
+
+def sphere_area(r):
+    sphere_area = 4 * 3.14 * r ** 2
+    return sphere_area
+s_area = sphere_area(3)
